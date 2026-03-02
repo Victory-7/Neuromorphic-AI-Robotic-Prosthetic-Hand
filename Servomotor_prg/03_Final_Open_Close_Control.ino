02e0fe3d8ca959da046ab39e50aca60a0e156f25
suhanii293 <suhani.pare@gmail.com>
2026-03-03 00:18:48 +0530

    Implement finger control with servo motor
    This program controls a servo motor to open and close a finger using calibrated angles.

--- Servomotor_prg/03_Final_Open_Close_Control.ino
+++ Servomotor_prg/03_Final_Open_Close_Control.ino
@@ -1,1 +1,39 @@
+/*
+  --------------------------------------------------------
+  Finger Control
+  File: 03_Final_Open_Close_Control.ino
+  Board: ESP32 S3 Mini
+  Servo: MG995
+  Purpose: Control finger using calibrated open
+           and close angles
+  --------------------------------------------------------
+*/
 
+#include <Servo.h>
+
+Servo fingerServo;
+const int servoPin = 5;
+
+// Update these values after calibration
+const int openAngle  = 10;
+const int closeAngle = 110;
+
+void setup() {
+  Serial.begin(115200);
+  fingerServo.attach(servoPin);
+
+  Serial.println("Finger Control System Initialized.");
+}
+
+void loop() {
+
+  // Open Finger
+  fingerServo.write(openAngle);
+  Serial.println("Finger Opened.");
+  delay(3000);
+
+  // Close Finger
+  fingerServo.write(closeAngle);
+  Serial.println("Finger Closed.");
+  delay(3000);
+}
